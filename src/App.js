9fa4295c6f40b0e60133e47011883427aa3f2b66
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-05-12 20:49:17 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -12,20 +12,31 @@ class App extends React.Component {
     imagem: '',
     raridade: 'normal',
     superTrunfo: false,
+    isSaveButtonDisabled: true,
+  }
+
+  validateForm = () => {
+    const { name, descricao, imagem, raridade, attr1, attr2, attr3 } = this.state;
+    let flag = 0;
+
+    if (name && descricao && imagem && raridade)flag += 1;
+    if (+attr1 + +attr2 + +attr3 <= +'210') flag += 1;
+    if ([+attr1, +attr2, +attr3]
+      .filter((num) => num >= 0 && num < +'91').length === +'3') flag += 1;
+    this.setState({ isSaveButtonDisabled: flag !== +'3' });
   }
 
   onInputChange =({ target }) => {
     const { name, type } = target;
     const value = type === 'checkbox' ? target.checked : target.value;
 
-    this.setState({
-      [name]: value,
-    });
+    this.setState(() => ({ [name]: value }), this.validateForm);
   }
 
   render() {
     const {
       name, descricao, attr1, attr2, attr3, imagem, raridade, superTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
@@ -38,6 +49,7 @@ class App extends React.Component {
           cardImage={ imagem }
           cardRare={ raridade }
           cardTrunfo={ superTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
           onInputChange={ this.onInputChange }
         />
         <Card
